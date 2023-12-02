c6a3955d9e41b6c9c295d6d6bb26cc6745bbe914
moashraf <figoashraf@gmail.com>
2023-12-02 11:09:36 +0200

    data

---
+++ app/Models/School/Meetings/meeting_agenda.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\School\Meetings;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class meeting_agenda extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/School/Meetings/meeting_recommendations.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class meeting_recommendations extends Model
+{
+    use HasFactory;
+}
